960848624e5f11df9267c831a8543060a72d35e3
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-02-01 13:08:34 -0300

    feature: criação do serviço de posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsRepository } from './repositories/posts.repository';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+
+  constructor(
+    private readonly repository: PostsRepository
+  ){}
+
+
+  async create(createPostDto: CreatePostDto) {
+    return await this.repository.create(createPostDto)
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll() {
+    return await this.repository.findAll()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    return await this.repository.findOne(id)
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    return await this.repository.update(id, updatePostDto)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number) {
+    return await this.repository.remove(id)
   }
 }
